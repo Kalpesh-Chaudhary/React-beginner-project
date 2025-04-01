e598bf7760d3d0c1871777f18db71b32898d5dae
kalpesh chaudhary <kalpeshchaudhary713@gmail.com>
2025-04-01 23:26:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Resturant from './Component/Resturant'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <Resturant/>
+   
+    </div>
+  )
+}
+
+export default App
